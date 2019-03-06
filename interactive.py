2d9b47520dbff6c0cb1754edc1d36d60c9b6f274
Stephen Mayhew <swm.mayhew@gmail.com>
2019-03-06 12:30:37 -0500

    interactive.py is now actually interactive

--- interactive.py
+++ interactive.py
@@ -8,7 +8,6 @@ from mylib import legal_predictor
 
 json_conv = JsonConverter()
 constitution = json_conv._read_const("data")
-print(constitution.keys())
 
 ldr = legal_reader.LegalDatasetReader()
 
@@ -18,17 +17,15 @@ model.eval()
 
 predictor = Predictor.by_name("legal_predictor")(model, ldr)
 
-#graf = "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press;"
-graf = "i'm stephen mayhew and i'm a little crazy"
-#amendment_keys = {'2', '7', '9', '10', '11', '14', '19', '23', '24', '27', '28', '32', '33', '34', '37', '42', '44',
-                  #'46', '49', '50', '54', '57', '59', '61', '62', '64', '65'}
 
-for k in constitution:
-    #if k not in amendment_keys:
-     #   continue
-    const = constitution[k]
-    out = predictor.predict_json({"graf": graf, "const": const})
-    if out["instance"]["prediction"] == 1:
-        print(k, out)
-        print(constitution[k])
-        #print(" ".join(map(str, constitution[k][:30])))
+while True:
+    graf = input("enter text>> ")
+    if graf in ["q", "quit"]:
+        break
+
+    for k in constitution:
+        const = constitution[k]
+        out = predictor.predict_json({"graf": graf, "const": const})
+        if out["instance"]["prediction"] == 1:
+            print(k, out)
+            print(constitution[k])
