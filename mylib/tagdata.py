1ef32155341ba49af0bcf170c28602f13eeecaf3
Stephen Mayhew <swm.mayhew@gmail.com>
2019-03-15 14:48:47 -0400

    added a remove clause to the data

--- mylib/tagdata.py
+++ mylib/tagdata.py
@@ -4,50 +4,57 @@ from collections import Counter
 import tqdm
 
 
-def tagdata(infile, outfile):
+def tagdata(infile, outfile, destructive=False, remove=False):
     with open("mylib/rules.txt") as f:
         lines = f.readlines()
         rules = dict(map(lambda line: line.strip().split("\t"), lines))
-
-    print("rules", rules)
 
     pat = re.compile('(' + '|'.join(rules.keys()) + ')')
 
     oldcounter = Counter()
     newcounter = Counter()
 
-    seen = set()
-
     with open(infile) as f, open(outfile, "w") as out:
         for line in tqdm.tqdm(f):
             text, origlabel = line.strip().split("\t")
             oldcounter[origlabel] += 1
             m = set(pat.findall(text.lower()))
-            if len(m) > 0:
-                newrules = set()
-                if origlabel != "unmatched":
-                    newrules.add(origlabel)
 
+            newrules = set()
+
+            if destructive:
+                newrules.add("unmatched")
+            else:
+                newrules.add(origlabel)
+
+            if len(m) > 0:
                 for matching_key in m:
+                    if remove:
+                        text = re.sub(matching_key, '', text, flags=re.IGNORECASE).strip()
                     newlabel = rules[matching_key]
                     newcounter[newlabel] += 1
                     newrules.add(newlabel)
 
-                outstring = "{}\t{}\n".format(text, ",".join(newrules))
-                out.write(outstring)
-            else:
-                out.write(line)
-                newcounter[origlabel] += 1
-    print("tag stats before:", oldcounter)
-    print("tag stats after:", newcounter)
+            if len(newrules) > 1 and "unmatched" in newrules:
+                newrules.remove("unmatched")
+
+            outstring = "{}\t{}\n".format(text, ",".join(newrules))
+            out.write(outstring)
+
+    #print("tag stats before:", oldcounter)
+    #print("tag stats after:", newcounter)
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='Process some rules.')
-    parser.add_argument('--infile', '-i', help='File of lines to read in, probably called tmp_lines')
-    parser.add_argument('--outfile', '-o', help='File to write to.')
+    parser.add_argument('--infile', '-i', help='File of lines to read in, probably called tmp_lines', required=True)
+    parser.add_argument('--outfile', '-o', help='File to write to.', required=True)
+    parser.add_argument("--destructive", "-d", help="Keep original rules or no?", default=False, action="store_true")
+    parser.add_argument("--remove", "-r", help="Remove matched string spans (to avoid model overfitting)", default=False, action="store_true")
+
 
     args = parser.parse_args()
-    tagdata(args.infile, args.outfile)
+    print(args)
+    tagdata(args.infile, args.outfile, args.destructive, args.remove)
